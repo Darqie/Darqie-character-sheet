8ee89ead1f734d55fbe4091dfedf9631e553d9a1
Darqie <ilevsheny@gmail.com>
2025-06-09 16:20:08 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
+import { initDiceBox, rollDie } from './dice/dicebox.js'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
